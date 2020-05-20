39ca2f72895a2c1bcad58285b50dbd2c995dd87a
Everton Ataide <everton.ataide@vtex.com.br>
2020-05-20 14:31:14 -0300

    Remove console.log

--- node/middlewares/customer.ts
+++ node/middlewares/customer.ts
@@ -2,50 +2,51 @@ import json from 'co-body'
 import { UserInputError } from '@vtex/api'
 
 declare let process: {
-    env: {
-        VTEX_APP_ID: string
-    }
+  env: {
+    VTEX_APP_ID: string
+  }
 }
 
 const IntegerLimit = 4294967295
 
-function generateId(min: number): Number {
-    var max = Math.floor(IntegerLimit)
-    return (Math.floor(Math.random() * (max - min)) + min)
+function generateId(min: number): number {
+  const max = Math.floor(IntegerLimit)
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
 export async function newCustomerId(ctx: Context, next: () => Promise<any>) {
-    const {
-        clients: { apps, masterdata },
-    } = ctx
+  const {
+    clients: { apps, masterdata },
+  } = ctx
 
-    let body = await json(ctx.req)
-    const appId = process.env.VTEX_APP_ID
-    let { lastCustomerId } = await apps.getAppSettings(appId)
+  const body = await json(ctx.req)
+  const appId = process.env.VTEX_APP_ID
+  const { lastCustomerId } = await apps.getAppSettings(appId)
 
-    ctx.status = 200
+  ctx.status = 200
 
-    let customerId = generateId(Math.ceil(parseInt(lastCustomerId)))
+  const customerId = generateId(Math.ceil(parseInt(lastCustomerId, 10)))
 
-    let bodyToPatch = { ...body, customerId: customerId }
-    try {
-        await masterdata.updateDocument(bodyToPatch)
+  const bodyToPatch = { ...body, customerId }
+  try {
+    await masterdata.updateDocument(bodyToPatch)
+  } catch (error) {
+    if (
+      error.response.data.Message ===
+      'duplicate entry at indice: CL-AlternateKeyCustomerId'
+    ) {
+      try {
+        await masterdata.updateDocument({
+          ...bodyToPatch,
+          customerId: generateId(Math.ceil(parseInt(lastCustomerId, 10))),
+        })
+      } catch (innerError) {
+        throw new UserInputError(`Cannot update registry: ${body}`)
+      }
     }
-    catch (error) {
-        console.log('primeiro error', error)
-        if (error.response.data.Message === 'duplicate entry at indice: CL-AlternateKeyCustomerId') {
-            try {
-                console.log('Vai tentar dar o update de novo')
-                await masterdata.updateDocument({ ...bodyToPatch, customerId: generateId(Math.ceil(parseInt(lastCustomerId))) })
-            }
-            catch (error) {
-                console.log(error)
-                throw new UserInputError(`Cannot update registry: ${body}`)
-            }
-        }
-    }
+  }
 
-    ctx.body = bodyToPatch
+  ctx.body = bodyToPatch
 
-    await next()
+  await next()
 }
