866377c867474b20150c2128e0ee145617249e5d
Everton Ataide <everton.ataide@vtex.com.br>
2020-05-19 23:58:43 -0300

    Hook and patch working

--- node/clients/index.ts
+++ node/clients/index.ts
@@ -1,3 +1,9 @@
 import { IOClients } from '@vtex/api'
+import { MasterData } from './masterdata';
 
-export class Clients extends IOClients {}
+export class Clients extends IOClients {
+    public get masterdata(): MasterData {
+        return this.getOrSet('serpro', MasterData)
+    }
+}
+
---
+++ node/clients/masterdata.ts
@@ -0,0 +1,35 @@
+import {
+    InstanceOptions,
+    IOContext,
+    ExternalClient,
+} from '@vtex/api'
+
+import { statusToError } from '../utils'
+
+export class MasterData extends ExternalClient {
+    public constructor(ctx: IOContext, options?: InstanceOptions) {
+        super(`http://api.vtex.com/${ctx.account}/dataentities`, ctx, {
+            ...options,
+            headers: {
+                ...(options && options.headers),
+                ...{ Accept: 'application/vnd.vtex.ds.v10+json' },
+                ...(ctx.adminUserAuthToken
+                    ? { VtexIdclientAutCookie: ctx.adminUserAuthToken }
+                    : null),
+                ...(ctx.storeUserAuthToken
+                    ? { VtexIdclientAutCookie: ctx.storeUserAuthToken }
+                    : null),
+            },
+        })
+    }
+
+    public updateDocument = (
+        fields: object
+    ) => this.patch('CL/documents', fields)
+
+
+    protected patch = <T>(url: string, data?: any) => {
+        return this.http.patch<T>(url, data).catch(statusToError)
+    }
+
+}
